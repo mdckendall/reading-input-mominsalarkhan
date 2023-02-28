ba9ffa91b059d228a9b9cd61ae6ad68474dca4df
Momin Salar <124016292+mominsalarkhan@users.noreply.github.com>
2023-02-28 03:34:00 +0000

    Added a scanner and returned the length.

--- Main.java
+++ Main.java
@@ -1,6 +1,16 @@
+import java.util.*;
+
 class Main {
+  
+  
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
     System.out.println("Enter a word:");
+    String Dino = sc.nextLine();
+    System.out.print(Dino.length());
+    
+    
+    
     
     //Your code will return the length of the entered String
   }
